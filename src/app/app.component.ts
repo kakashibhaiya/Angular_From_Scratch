dfb21124ff92c2b5b3651e4bda626e86ee82c244
kakashibhaiya <chetanflash15@gmail.com>
2025-03-03 01:20:26 +0530

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AdminComponent } from './components/admin/admin.component';
+import { UserComponent } from './components/user/user.component';
+//import { DatabindingComponent } from './databinding/databinding.component';
+//import { StructuralDirComponent } from './components/directive/structural-dir/structural-dir.component';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [AdminComponent, UserComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  standalone: true
 })
 export class AppComponent {
   title = 'my-angular-app';
